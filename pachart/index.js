709f29dd96d970ede447f0e22476c593142da540
menooing.dev <menooing.dev@gmail.com>
2019-01-10 15:33:50 -0600

    add

---
+++ pachart/index.js
@@ -0,0 +1,47 @@
+import ComponentB from 'ComponentB.vue';
+
+
+
+var tempData = {
+	name:'kim',
+	date:'2222',
+	ch1:'good',
+	ch2:'mi',
+	ch3:'d2'
+
+};
+
+Vue.component('component-a', ComponentB);
+
+new Vue({
+    el: '#main_content',
+    data: { activetab: 1 ,pa:tempData,    errors: [],
+    name: null,
+    email: null,
+    movie: null
+  },
+  methods: {
+    checkForm: function (e) {
+      this.errors = [];
+
+      if (!this.name) {
+        this.errors.push("Name required.");
+      }
+      if (!this.email) {
+        this.errors.push('Email required.');
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.');
+      }
+
+      if (!this.errors.length) {
+        return true;
+      }
+
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+});
